21b6f2dbafbb1338e4fba2755807fcd1c773e848
Willem Berner <willem.berner@gmail.com>
2026-03-07 14:03:30 +0100

    Changes

--- resample.py
+++ resample.py
@@ -7,7 +7,7 @@ from pathlib import Path
 ROOT = Path("/mnt/sda/willem/datasets/seamless_interaction") #Linux
 #ROOT = Path("/Users/willemberner/datasets/seamless_interaction") #Mac
 
-# Optional: Remove WAV's for reducing storage. 
+# Optional: Remove MP3's for reducing storage. 
 
 sr = 16000 
 PCM_CODEC = "pcm_s16le"
