ee3bf8241ab6d20f71a3b0aeddc5bd6d95c8e927
Willem Berner <willem.berner@gmail.com>
2026-03-10 09:33:52 +0100

    Small changes

--- resample.py
+++ resample.py
@@ -1,9 +1,13 @@
+## WB
+
 
 import subprocess
 from pathlib import Path 
 
 ROOT = Path("/mnt/sda/willem/datasets/seamless_interaction") #Linux
 #ROOT = Path("/Users/willemberner/datasets/seamless_interaction") #Mac
+
+# Optional: Remove MP4's for reducing storage. 
 
 sr = 16000 
 PCM_CODEC = "pcm_s16le"
@@ -14,14 +18,13 @@ def resample_file(path: Path):
     cmd = ["ffmpeg", "-y", "-i", str(path), "-ar", str(sr), "-c:a", PCM_CODEC, str(tmp)]
 
     subprocess.run(cmd, check=True)
-    tmp.replace(path) 
+    tmp.replace(path)  
 
 
 def main(): 
     wavs = list(ROOT.rglob("*.wav"))
     for i, wav in enumerate(wavs): 
         resample_file(wav)
-
 
 if __name__ == "__main__":
     main()
